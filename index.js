a203727134e69d2fd73087348fff4e9128cf60de
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-06-13 09:00:18 +0500

    file route setup

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const db = require("./config/db");
+const files = require("./routes/files");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 db();
 
+app.use(express.json());
+// Routes
+app.use("/api/file", files);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

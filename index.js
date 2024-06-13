8b98fd886ec438c6a652805856e016ded433b448
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-06-13 09:35:59 +0500

    setup frontend and file download

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const db = require("./config/db");
 const files = require("./routes/files");
+const show = require("./routes/show");
 
 const app = express();
 const port = 3000;
@@ -8,8 +10,15 @@ const port = 3000;
 db();
 
 app.use(express.json());
+app.use(express.static("public"));
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
 // Routes
 app.use("/api/file", files);
+app.use("/file", show);
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
